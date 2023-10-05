e3f453dd10e062362b16a91cede9a0952f1adfc8
OEURN SEYHA <seyhaoeurn920@gmail.com>
2023-10-05 16:20:57 +0700

    update

--- src/app/user/[id]/page.js
+++ src/app/user/[id]/page.js
@@ -1,15 +1,27 @@
-import React from 'react'
+import React from "react";
 
+export default async function page({ params }) {
+  console.log(params.id);
 
-function page({params}) {
-    console.log(params.id)
-    
+  const response = await fetch(
+    `https://jsonplaceholder.typicode.com/posts/${params.id}`,{next : {
+        revalidate: 10
+      }}
+  );
+  const data = await response.json();
+
   return (
     <div>
-        {params.id}
-
+ 
+     {
+      console.log([data] )
+        // data.title
+     }
+      {[data].map((item) => (
+        <div key={item.id} className=" font-bold">
+          {item.title}
+        </div>
+      ))}
     </div>
-  )
+  );
 }
-
-export default page
